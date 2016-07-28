eafd2d61538a7feecd3be3006789972b901fcf07
Alaleh Azhir <AlalehAzhir@alalehs-mbp.private.wireless.med.harvard.edu>
2016-07-28 12:53:16 -0400

    hover feature

--- app/scripts/WigglePixiLine.js
+++ app/scripts/WigglePixiLine.js
@@ -18,10 +18,11 @@ export function WigglePixiLine() {
     var trackH = 50;
     var shownT = {};
 
-
+    
     function tileId(tile) {
         // uniquely identify the tile with a string
-        return tile.join(".") + '.' + tile.mirrored;
+        return tile.tileId;
+       // return tile.join(".") + '.' + tile.mirrored;
     }
 
     function loadTileData(tile_value) {
@@ -53,8 +54,13 @@ export function WigglePixiLine() {
                 d.resizeDispatch = resizeDispatch == null ? d3.dispatch('resize') : resizeDispatch;
             }
 
+            if (!('loadedTiles' in d))
+            {
+                d.loadedTiles = [];
+            }
+
             if (!('translate' in d)) {
-                d.translate = [0,0];
+                d.translate = [0,0]; 
             }
 
             if (!('scale' in d)) {
@@ -77,22 +83,44 @@ export function WigglePixiLine() {
                 d.preHeight = d.height; 
             }
 
+            if (!('totalWidth' in d)) {
+                d.totalWidth = 0; 
+            }
+
             if (!('shownT' in d)) {
                 d.shownT = {}; 
             }           
+
+
+            /*if (!('axis' in d)) {
+                d.svg = "";
+                d.y = "";
+                d.yAxis = "";
+            } */
 
             if (!('pMain' in d)) {
 
                 let pMain = new PIXI.Graphics();
                 d.stage.addChild(pMain);
-
+                
+                
                 d.pMain = pMain;
+                
 
             }
+
 
             if (!('chartType' in d)) {
                 d.chartType = "line";
             }
+
+            if (!('maxValue' in d)) {
+                let tileData = d3.select(this).selectAll('.tile-g').data();
+                
+                let maxVisibleValue = Math.max(...tileData.map((x) => x.valueRange[1]));
+                d.maxValue = maxVisibleValue;
+            }
+
 
             if (!('onchange' in d)) {
                 $("#mode input[name=type]").change(function () { 
@@ -101,26 +129,123 @@ export function WigglePixiLine() {
                     d.chartType = selectedChartType;
 
                     
-                    redraw("changed mode");
+                    redraw();
                 
                 
+                }); 
+              //  let tileData = d3.select(this).selectAll('.tile-g').data();
+                
+                //let maxVisibleValue = Math.max(...tileData.map((x) => x.valueRange[1]));
+                var ticks = Math.floor(d.height/10)-2;
+                d.y = d3.scale.linear().domain([0, d.maxValue])
+                .range([d.height,0]);
+               // .range([100,0])
+
+                d.yAxis = d3.svg.axis()
+                .ticks(ticks)
+                .orient("right") 
+                .scale(d.y);
+                d.w = $(this).css("width");
+                d.w = parseInt(d.w, 10);
+                d.svg = d3.select(this).append("svg").attr("id","svg").attr("height",d.height+10).style("margin-top","-5px").attr("viewBox", "0 -5 "+ d.w + " " + (d.height+10));
+                //svg = d3.select(this).append("svg").attr("height",100);
+                d.svg.append("g").attr("class", "y axis")/*.style("margin-top","5px")*/; 
+                d.svg.select('g.y.axis').call(d.yAxis); 
+
+
+                $(this).mousemove(function(e) { 
+
+                    $("#tooltip").empty();
+                    $('#tooltip').hide();
+                   // var parentOffset = $(this).parent().offset();
+                    var $div = $('.track');
+                    var mouse = {x:e.clientX,y:e.clientY};
+                    var top = $(this).css("top");
+                    top = parseInt(top, 10);
+                    var div = {x:$div.offset().left - $(window).scrollLeft(),y:$div.offset().top - $(window).scrollTop()};
+               
+                    let xPos = xScale.invert((mouse.x - div.x - d.translate[0])/d.scale);
+                    let tileX, length, index, val;
+                    let maxVisibleValue, ypos;
+                    if(mouse.y-div.y-top > (d.height+5)){
+                        
+                        d.pMain.removeChild(d.tileGraphics[10]);
+                        if(d.tileGraphics[10] != null){
+                           d.tileGraphics[10].destroy();
+                        }
+                        delete d.tileGraphics[10];
+
+                        return;
+                    }
+                    for(let tileId in d.shownT) {
+                
+                        var substring = tileId.split("/");
+                        
+                        length = d.totalWidth/Math.pow(2, parseInt(substring[0]));
+                       // console.log(length);
+                        //tile's starting point
+                        tileX = length*parseInt(substring[1]);
+                        //console.log(tileX);
+                        if(xPos >= tileX && xPos < tileX+length){
+                            //d.loadedTiles[tileId];  
+
+                            index = Math.floor((xPos - tileX)/length * 256);
+                            val = d.loadedTiles[tileId][index]; 
+                           // maxVisibleValue = Math.max(...tileData.map((x) => x.valueRange[1]));
+                            
+                            if(val != null){
+                                $('#tooltip').show();
+                                $("#tooltip").css('top', (e.pageY+10)+'px');
+                                $("#tooltip").css('left', (e.pageX+10)+'px');
+
+                                $("#tooltip").append("<span>The value at " + Math.floor(xPos) + " is " + Math.floor(val*100)/100 + ".</span>");
+                                 d.pMain.removeChild(d.tileGraphics[10]);
+                                // d.hover.removeChild(d.tileGraphics[10]);
+                                    if(d.tileGraphics[10] != null){
+                                       d.tileGraphics[10].destroy();
+                                    }
+                                    delete d.tileGraphics[10];
+                                    let graphics = new PIXI.Graphics();
+                                    ypos = val/d.maxValue;
+                                if(d.chartType == "line" || d.chartType == "point"){
+                                    
+                                    //graphics.lineStyle(1, 0xFF0000, 1);
+                                    graphics.beginFill(0xFF0000);
+                                    
+                                    graphics.drawRect(Math.floor((mouse.x - div.x)-d.translate[0]-2)-1, d.height-d.height*ypos-1, 3,3);
+                                    graphics.endFill();
+                                } else if(d.chartType == "bar"){
+                                    graphics.beginFill(0xFF0000);
+                                   // graphics.beginFill(0xC0C0C0);
+                                    graphics.drawRect(Math.floor((mouse.x - div.x)-d.translate[0])/d.scale, d.height-d.height*ypos, xScale(length/256),d.height*ypos);
+                                    graphics.endFill();
+                                 //   d.hover.addChild(graphics);
+                                }
+                                d.pMain.addChild(graphics);
+                                    
+                                d.tileGraphics[10] = graphics;
+
+
+                            }
+                            
+                        }
+                        
+
+                    }
+                   // console.log($(this).css("top"));
+                }); 
+
+                $(this).mouseleave(function(e) {
+                    $('#tooltip').empty();
+                    $('#tooltip').hide();
+                    d.pMain.removeChild(d.tileGraphics[10]);
+                                    if(d.tileGraphics[10] != null){
+                                       d.tileGraphics[10].destroy();
+                                    }
+                                    delete d.tileGraphics[10];
                 });
-                if(m == 0){
-                   $(".track").mousemove(function(e) {
-                       // var parentOffset = $(this).parent().offset();
-                        var $div = $('.track');
-                        var mouse = {x:e.clientX,y:e.clientY};
-                        var top = $(this).css("top");
-                        top = parseInt(top, 10);
-                        var div = {x:$div.offset().left - $(window).scrollLeft(),y:$div.offset().top - $(window).scrollTop()};
-                        console.log('x:' + (mouse.x - div.x) + ' y:' + (mouse.y - div.y - top));
-                        console.log(Math.floor((mouse.x - div.x - d.translate[0])/d.scale));
-                       // console.log(d.xPoints)
-                       // console.log($(this).css("top"));
-                    }); 
-                    m++;
-                }
-                
+                   //m++;
+                //}
                 d.onchange = true;
             }
 
@@ -133,11 +258,15 @@ export function WigglePixiLine() {
                 d.yPoints = [];
             }                  
 
+
             function redrawTile() {
-                console.log("redraw called")
+               // console.log("redraw called");
                 let tileData = d3.select(this).selectAll('.tile-g').data();
                 let minVisibleValue = Math.min(...tileData.map((x) => x.valueRange[0]));
                 let maxVisibleValue = Math.max(...tileData.map((x) => x.valueRange[1]));
+                d.maxValue = maxVisibleValue;
+
+                
 
 
                 let yScale = d3.scale.linear()
@@ -145,7 +274,9 @@ export function WigglePixiLine() {
                 //.domain([0, maxVisibleValue])
                 .range([0, 1]);
 
-                
+                d.y.domain([0, maxVisibleValue]); 
+                d.svg.select('g.y.axis').call(d.yAxis);
+
                 let reset = function(){
                     d.xPoints = [];
                     d.yPoints = [];
@@ -155,10 +286,24 @@ export function WigglePixiLine() {
 
                 let drawTile = function(graphics, tile) {
 
+
+                    let tileData = [];
+
+                    if(tileId(tile) in d.loadedTiles)
+                    {
+                        tileData = d.loadedTiles[tileId(tile)];
+                    }
+                    else
+                    {
+                        tileData = loadTileData(tile.data);
+                        d.loadedTiles[tileId(tile)] = tileData;
+                    }
+
                     //console.log('drawing tile:', tile.tileId, xScale.domain(), xScale.range());
-                    let tileData = loadTileData(tile.data);
+                    //let tileData = loadTileData(tile.data);
 
                     let tileWidth = (tile.xRange[1] - tile.xRange[0]) / Math.pow(2, tile.tilePos[0]);
+                    d.totalWidth = tile.xRange[1] - tile.xRange[0];
                     // this scale should go from an index in the data array to 
                     // a position in the genome coordinates
                     let tileXScale = d3.scale.linear().domain([0, tileData.length])
@@ -172,7 +317,8 @@ export function WigglePixiLine() {
                         graphics.lineStyle(1, 0x0000FF, 1);
                     } else if(d.chartType == "bar") {
                         graphics.lineStyle(0, 0x0000FF, 1);
-                        graphics.beginFill(0xFF700B, 1);
+                        //graphics.beginFill(0xFF700B, 1);
+                        graphics.beginFill(0xA9A9A9, 1);
                     } else if(d.chartType == "heatmap") {
                         color=d3.scale.linear()
                             .domain([0,1])
@@ -180,9 +326,9 @@ export function WigglePixiLine() {
                     }
                     
                     let j = 0;
-
+                    
                     for (let i = 0; i < tileData.length; i++) {
-
+                        //console.log(tileXScale(i));
 
                         let xPos = xScale(tileXScale(i));
                         //let yPos = -(d.height - yScale(tileData[i]));
@@ -210,9 +356,7 @@ export function WigglePixiLine() {
                         } else if(d.chartType == "heatmap"){
 
                           
-                                if(color(height).replace("#","0x") == null){
-                                    console.log("this is why im broken")
-                                }
+                                
                                 graphics.beginFill(color(height).replace("#","0x"), 1);
                                 graphics.drawRect(xPos, 0, width, d.height);
                                 graphics.endFill();
@@ -230,6 +374,7 @@ export function WigglePixiLine() {
                     }
                     graphics.endFill(); 
                     //console.log(xPoints);
+                   
                 }
 
 
@@ -238,6 +383,9 @@ export function WigglePixiLine() {
                 let shownTiles = {};
                 d.shownT = {};
                 let k = 0;
+
+              
+              //  var text = new PIXI.Text(maxVisibleValue,{font : '11px Arial', fill : 0x000000, align : 'center'});
                 for (let i = 0; i < tileData.length; i++) {
                     shownTiles[tileData[i].tileId] = true;
                     d.shownT[tileData[i].tileId] = true;
@@ -260,6 +408,8 @@ export function WigglePixiLine() {
                          d.tileGraphics[tileData[i].tileId] = newGraphics
                     } 
                 }
+              //   d.pMain.addChild(text);
+               // console.log(d.xPoints);
 
                 for (let tileIdStr in d.tileGraphics) {
                     if (!(tileIdStr in shownTiles)) {
@@ -288,6 +438,16 @@ export function WigglePixiLine() {
                 //redraw();
                 
                 if(d.preHeight != d.height){
+                    d.y.range([d.height,0]);
+                    //$("#svg").height(d.height);
+                    if(Math.floor(d.height/10)-2 < 15 && Math.floor(d.height/10)-2 > 0){
+                        d.yAxis.ticks(Math.floor(d.height/10)-2);
+                    }
+                    
+                    d.svg.attr("height", d.height+10);
+                    d.svg.attr("viewBox", "0 -5 "+ d.w + " " + (d.height+10))
+                    d.svg.select('g.y.axis').call(d.yAxis);
+
                     redraw();
                 }
 
@@ -296,6 +456,7 @@ export function WigglePixiLine() {
 
             
             function redraw(){
+               // svg.select("g.y.axis").call(yAxis);
                 d.pMain.clear();
                 d.pMain.position.x = d.translate[0];
                 for (let tileIdStr in d.tileGraphics) {
@@ -321,7 +482,8 @@ export function WigglePixiLine() {
                 if(d.chartType == "bar"){
                     
                     graphics.lineStyle(0, 0x0000FF, 1);
-                    graphics.beginFill(0xFF700B, 1);
+                   // graphics.beginFill(0xFF700B, 1);
+                   graphics.beginFill(0xA9A9A9, 1);
 
                 } else if(d.chartType == "line" || d.chartType == "point"){
 
@@ -357,8 +519,8 @@ export function WigglePixiLine() {
                         graphics.drawRect(d.xPoints[i]*d.scale, d.height-d.yPoints[i]*d.height, 1, 1);
                            
                     } else if(d.chartType == "bar"){
-                        graphics.beginFill(0xFF700B, 1);
-                       
+                        //graphics.beginFill(0xFF700B, 1);
+                        graphics.beginFill(0xA9A9A9, 1);
                         graphics.drawRect(d.xPoints[i], d.height-d.yPoints[i]*d.height, d.xPoints[i+1]-d.xPoints[i], d.height*d.yPoints[i]);
                
 
@@ -397,12 +559,10 @@ export function WigglePixiLine() {
                 d.scale = scale;
                 
 
-               // console.log(d.translate[0]);
                 d.pMain.position.x = d.translate[0];
 
                 if(d.chartType == "bar" || d.chartType == "heatmap"){
                     d.pMain.scale.x = scale;
-              //      sizeChanged();
                     return;
                 }
                 
